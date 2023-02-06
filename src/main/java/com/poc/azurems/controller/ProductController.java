41ccb6939c044e6fa67c84d0b22eeab36d9bee71
USER <USER@192.168.99.1>
2023-02-05 23:59:31 -0500

    change in product controller

--- src/main/java/com/poc/azurems/controller/ProductController.java
+++ src/main/java/com/poc/azurems/controller/ProductController.java
@@ -2,19 +2,23 @@ package com.poc.azurems.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.poc.azurems.model.Product;
 import com.poc.azurems.repo.ProductRepo;
 
+@SpringBootApplication
 @RestController
 public class ProductController {
-
+	@Autowired
 	private  ProductRepo repo;
 	@PostMapping("/product")
-	public   Product saveProduct(Product prod) {
+	public   Product saveProduct(@RequestBody Product prod) {
 		return repo.save(prod);
 		
 	}
